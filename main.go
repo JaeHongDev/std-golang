cdf0eed58121fe670cacb275806c5ca8f0cd025c
JaeHongDev <println@kakao.com>
2021-06-08 13:54:04 +0000

    20210608

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import "fmt"
-
-func PrintWords(words ...string) (returnWords []string) {
-	returnWords = words
-	return
-}
 func main() {
 
-	str := PrintWords("a", "b", "c")
-
-	for i := 0; i < len(str); i++ {
-		fmt.Println(str[i])
-	}
-
-	for index, item := range str {
-		fmt.Println(index, item)
-	}
 }
